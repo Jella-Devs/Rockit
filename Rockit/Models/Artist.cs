0670619f2c2e8c43e18e8b0b16d4e823747e180f
Leal Oliva Carlos Fernando <paintsoliva@gmail.com>
2025-05-21 19:36:57 -0700

    Modelos agregados

---
+++ Rockit/Models/Artist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rockit.Models
+{
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int Players { get; set; }
+
+        [Required]
+        public string Front { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; } = new List<Song>();
+    }
+}
---
+++ Rockit/Models/Song.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rockit.Models
+{
+    public class Song
+    {
+        public int SongId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Route { get; set; }
+
+        public int Players { get; set; }
+
+        public int ArtistId { get; set; }
+
+        public virtual Artist Artist { get; set; }
+    }
+}
