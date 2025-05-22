2fc59c9562eb527327525c9cc79dec51187bcc95
Leal Oliva Carlos Fernando <paintsoliva@gmail.com>
2025-05-21 19:52:08 -0700

    Se agrego entity framework, se agrego DbContext

---
+++ Rockit/Data/RockolaDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rockit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rockit.Data
+{
+    public class RockolaDbContext : DbContext
+    {
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Song> Songs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+     .UseLazyLoadingProxies()
+     .UseNpgsql("Host=localhost;Port=5432;Database=rockola_db;Username=rockola_user;Password=rockola_pass");
+
+        }
+    }
+}
