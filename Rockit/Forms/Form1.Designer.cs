61e7f6d3457f6fce06465d843e4b7e81a48b04e9
AlanGuemes <alanguemesii@hotmail.com>
2025-05-21 23:56:39 -0700

    Cursor de menu y despliegue de artistas

--- Rockit/Forms/Form1.Designer.cs
+++ Rockit/Forms/Form1.Designer.cs
@@ -37,6 +37,14 @@
             picArtist7 = new PictureBox();
             picArtist6 = new PictureBox();
             picArtist5 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
             ((System.ComponentModel.ISupportInitialize)picArtist1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)picArtist2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)picArtist3).BeginInit();
@@ -59,7 +67,7 @@
             // 
             // picArtist1
             // 
-            picArtist1.Location = new Point(73, 115);
+            picArtist1.Location = new Point(98, 132);
             picArtist1.Name = "picArtist1";
             picArtist1.Size = new Size(90, 90);
             picArtist1.TabIndex = 1;
@@ -67,7 +75,7 @@
             // 
             // picArtist2
             // 
-            picArtist2.Location = new Point(248, 115);
+            picArtist2.Location = new Point(273, 132);
             picArtist2.Name = "picArtist2";
             picArtist2.Size = new Size(90, 90);
             picArtist2.TabIndex = 2;
@@ -75,7 +83,7 @@
             // 
             // picArtist3
             // 
-            picArtist3.Location = new Point(405, 115);
+            picArtist3.Location = new Point(430, 132);
             picArtist3.Name = "picArtist3";
             picArtist3.Size = new Size(90, 90);
             picArtist3.TabIndex = 3;
@@ -83,7 +91,7 @@
             // 
             // picArtist4
             // 
-            picArtist4.Location = new Point(583, 115);
+            picArtist4.Location = new Point(608, 132);
             picArtist4.Name = "picArtist4";
             picArtist4.Size = new Size(90, 90);
             picArtist4.TabIndex = 4;
@@ -91,7 +99,7 @@
             // 
             // picArtist8
             // 
-            picArtist8.Location = new Point(583, 261);
+            picArtist8.Location = new Point(608, 278);
             picArtist8.Name = "picArtist8";
             picArtist8.Size = new Size(90, 90);
             picArtist8.TabIndex = 8;
@@ -99,7 +107,7 @@
             // 
             // picArtist7
             // 
-            picArtist7.Location = new Point(405, 261);
+            picArtist7.Location = new Point(430, 278);
             picArtist7.Name = "picArtist7";
             picArtist7.Size = new Size(90, 90);
             picArtist7.TabIndex = 7;
@@ -107,7 +115,7 @@
             // 
             // picArtist6
             // 
-            picArtist6.Location = new Point(248, 261);
+            picArtist6.Location = new Point(273, 278);
             picArtist6.Name = "picArtist6";
             picArtist6.Size = new Size(90, 90);
             picArtist6.TabIndex = 6;
@@ -115,17 +123,97 @@
             // 
             // picArtist5
             // 
-            picArtist5.Location = new Point(73, 261);
+            picArtist5.Location = new Point(98, 278);
             picArtist5.Name = "picArtist5";
             picArtist5.Size = new Size(90, 90);
             picArtist5.TabIndex = 5;
             picArtist5.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(123, 225);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 9;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(295, 225);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 10;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(453, 225);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 11;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(635, 225);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 12;
+            label4.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(123, 371);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 13;
+            label5.Text = "label5";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(295, 371);
+            label6.Name = "label6";
+            label6.Size = new Size(38, 15);
+            label6.TabIndex = 14;
+            label6.Text = "label6";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(453, 371);
+            label7.Name = "label7";
+            label7.Size = new Size(38, 15);
+            label7.TabIndex = 15;
+            label7.Text = "label7";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(635, 371);
+            label8.Name = "label8";
+            label8.Size = new Size(38, 15);
+            label8.TabIndex = 16;
+            label8.Text = "label8";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(picArtist8);
             Controls.Add(picArtist7);
             Controls.Add(picArtist6);
@@ -150,6 +238,7 @@
             ((System.ComponentModel.ISupportInitialize)picArtist6).EndInit();
             ((System.ComponentModel.ISupportInitialize)picArtist5).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -163,5 +252,13 @@
         private PictureBox picArtist7;
         private PictureBox picArtist6;
         private PictureBox picArtist5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
     }
 }
--- Rockit/Forms/Form1.cs
+++ Rockit/Forms/Form1.cs
@@ -1,10 +1,15 @@
 using Rockit.Forms;
 using Rockit.Models;
+using System.Windows.Forms;
 
 namespace Rockit
 {
     public partial class Form1 : Form
     {
+        //Index para movimiento de paginas del menu
+        int pages;
+        int cursor = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -31,8 +36,13 @@ namespace Rockit
                 MessageBox.Show("Prev");
             }
             else if (e.KeyCode == Keys.Add)
-            {
-                MessageBox.Show("Next");
+            {   
+                if ((int)Math.Ceiling((double)cursor / 8) != pages - 1)
+                {
+                    cursor = cursor != 0 ? cursor + 8 : (cursor + 1) * 8;
+                    ClearMenu();
+                }
+                RefreshMenu();
             }
             else if (e.KeyCode == Keys.Multiply)
             {
@@ -43,20 +53,77 @@ namespace Rockit
 
         private void rshButton_Click(object sender, EventArgs e)
         {
+           RefreshMenu();
+        }
+        private void RefreshMenu()
+        {
             if (ArtistStore.ListOfArtist.Count > 0)
             {
-                MessageBox.Show("Hay carga " + ArtistStore.ListOfArtist.Count);
-                // Obtener los primeros 8 artistas (o menos si hay menos de 8)
-                var topArtists = ArtistStore.ListOfArtist
-                    .Take(8)
-                    .Select(a => a.Name);
+                pages = (int)Math.Ceiling((double)ArtistStore.ListOfArtist.Count / 8);
 
-                // Unirlos en un solo string con saltos de línea
-                string message = string.Join(Environment.NewLine, topArtists);
+                // Declarar elementos dinamicos
+                var pictureBoxes = new List<PictureBox>
+                {
+                    picArtist1, picArtist2, picArtist3, picArtist4,
+                    picArtist5, picArtist6, picArtist7, picArtist8
+                };
+                var labels = new List<Label>
+                {
+                    label1,label2,label3,label4,label5, label6, label7, label8
+                };
 
-                // Mostrar en un MessageBox
-                MessageBox.Show(message, "Primeros 8 Artistas");
+                // Tomar datos de la lista dependiendo la posicion del cursor
+                var topArtists = ArtistStore.ListOfArtist.Skip(cursor).Take(8).ToList();
+
+                // Asignar cada portada a su PictureBox
+                for (int i = 0; i < topArtists.Count; i++)
+                {
+                    string path = topArtists[i].Picture;
+                    string name = topArtists[i].Name;
+
+                    try
+                    {
+                        if (System.IO.File.Exists(path))
+                        {
+                            labels[i].Text = name;
+                            pictureBoxes[i].Image = Image.FromFile(path);
+                            pictureBoxes[i].SizeMode = PictureBoxSizeMode.StretchImage; // Ajuste al tamaño del PictureBox
+                        }
+                        else
+                        {
+                            labels[i].Text = null;
+                            pictureBoxes[i].Image = null;
+                        }
+                    }
+                    catch
+                    {
+                        // Puedes dejarlo en blanco o poner una imagen por defecto
+                        labels[i].Text = null;
+                        pictureBoxes[i].Image = null;
+                    }
+                }
             }
+        }
+        private void ClearMenu()
+        {
+            var pictureBoxes = new List<PictureBox>
+            {
+                picArtist1, picArtist2, picArtist3, picArtist4,
+                picArtist5, picArtist6, picArtist7, picArtist8
+            };
+
+            var labels = new List<Label>
+            {
+                label1, label2, label3, label4,
+                label5, label6, label7, label8
+            };
+
+            foreach (var pic in pictureBoxes)
+                pic.Image = null;
+
+            foreach (var lbl in labels)
+                lbl.Text = string.Empty;
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
