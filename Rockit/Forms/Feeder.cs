e7606e9b3eb553857e8a9e9920e9730698024ff4
AlanGuemes <alanguemesii@hotmail.com>
2025-05-21 19:51:08 -0700

    Creacion Feeder

---
+++ Rockit/Forms/Feeder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rockit.Forms
+{
+    public partial class Feeder : Form
+    {
+        public Feeder()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
